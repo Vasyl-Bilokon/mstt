2e7804669b859eb050d1f5bcadd73abe96b037c0
Vasyl-Bilokon <120991094+Vasyl-Bilokon@users.noreply.github.com>
2022-12-24 23:58:57 +0200

    Sample of Behaviour - JADE and Java.core

---
+++ src/Behaviours/Samples/MyOneShotBehaviour.java
@@ -0,0 +1,10 @@
+package Behaviours.Samples;
+
+import jade.core.behaviours.OneShotBehaviour;
+
+public class MyOneShotBehaviour extends OneShotBehaviour {
+    @Override
+    public void action() {
+        // TO-DO Something
+    }
+}
---
+++ src/Behaviours/Samples/OverbearingBehaviour.java
@@ -0,0 +1,18 @@
+package Behaviours.Samples;
+
+import jade.core.behaviours.Behaviour;
+
+public class OverbearingBehaviour extends Behaviour {
+	
+    @Override
+    public void action() {
+        while (true) {
+            // TO-DO something
+        }
+    }
+
+    @Override
+    public boolean done() {
+        return true;
+    }
+}
