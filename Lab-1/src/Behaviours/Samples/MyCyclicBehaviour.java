5c630cc5fbea9ddf4a2d2cd0bad545818c4df548
Vasyl-Bilokon <120991094+Vasyl-Bilokon@users.noreply.github.com>
2023-01-07 00:01:26 +0200

    Sample of Behaviour - JADE and Java.core

---
+++ Lab-1/src/Behaviours/Samples/MyCyclicBehaviour.java
@@ -0,0 +1,10 @@
+package Behaviours.Samples;
+
+import jade.core.behaviours.CyclicBehaviour;
+
+public class MyCyclicBehaviour extends CyclicBehaviour {
+    @Override
+    public void action() {
+        // TO-DO something
+    }
+}
